fa6d76f0fb55bcaa00bb852daec7ac5d4282175b
Rushil Bhatt <121129252+rushilBhatt-lab@users.noreply.github.com>
2024-02-11 21:01:15 +0100

    added getUrlfromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,29 @@
+const { JSDOM } = require('jsdom');
+
+function getURLsfromHTML(htmlbody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlbody);
+  const linkElements = dom.window.document.querySelectorAll('a');
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === '/') {
+      try {
+        const urlObj = new URL(`${baseURL}${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`error with relative url: ${err.message}`);
+      }
+    } else {
+      try {
+        const urlObj = new URL(linkElement.href);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`error with aboslute url: ${err.message}`);
+      }
+    }
+  }
+  return urls;
+}
+
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString);
   const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -9,4 +35,5 @@ function normalizeURL(urlString) {
 
 module.exports = {
   normalizeURL,
+  getURLsfromHTML,
 };
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require('./crawl.js');
+const { normalizeURL, getURLsfromHTML } = require('./crawl.js');
 const { test, expect } = require('@jest/globals');
 
 test('normalizeURL remove protocol', () => {
@@ -26,5 +26,43 @@ test('normalizeURL remove HTTP', () => {
   const input = 'http://rushil-dev.vercel.app/path';
   const output = normalizeURL(input);
   const expectedOutput = 'rushil-dev.vercel.app/path';
+  expect(output).toEqual(expectedOutput);
+});
+
+test('getURLsfromHTML absolute URL ', () => {
+  const inputHtmlBody = `<html><body><a href="https://rushil-dev.vercel.app/path/">Rushil.dev</a></body></html>`;
+  const inputBaseURL = 'https://rushil-dev.vercel.app/path/';
+  const output = getURLsfromHTML(inputHtmlBody, inputBaseURL);
+  const expectedOutput = ['https://rushil-dev.vercel.app/path/'];
+  expect(output).toEqual(expectedOutput);
+});
+
+test('getURLsfromHTML relative URL', () => {
+  const inputHtmlBody = `<html><body><a href="/path/">Rushil.dev</a></body></html>`;
+  const inputBaseURL = 'https://rushil-dev.vercel.app';
+  const output = getURLsfromHTML(inputHtmlBody, inputBaseURL);
+  const expectedOutput = ['https://rushil-dev.vercel.app/path/'];
+  expect(output).toEqual(expectedOutput);
+});
+
+test('getURLsfromHTML relative UR', () => {
+  const inputHtmlBody = `
+  <html><body><a href="https://rushil-dev.vercel.app/path1/">Rushil.dev</a>
+  <a href="/path2/">Rushil.dev</a>
+  </body></html>`;
+  const inputBaseURL = 'https://rushil-dev.vercel.app';
+  const output = getURLsfromHTML(inputHtmlBody, inputBaseURL);
+  const expectedOutput = [
+    'https://rushil-dev.vercel.app/path1/',
+    'https://rushil-dev.vercel.app/path2/',
+  ];
+  expect(output).toEqual(expectedOutput);
+});
+
+test('getURLsfromHTML relative URL', () => {
+  const inputHtmlBody = `<html><body><a href="invalid">Invalid URL</a></body></html>`;
+  const inputBaseURL = 'https://rushil-dev.vercel.app';
+  const output = getURLsfromHTML(inputHtmlBody, inputBaseURL);
+  const expectedOutput = [];
   expect(output).toEqual(expectedOutput);
 });
