4d9dc205b68b5c228346730d0de9f957f5f699b7
Rushil Bhatt <121129252+rushilBhatt-lab@users.noreply.github.com>
2024-02-11 22:25:13 +0100

    working crawl page

--- crawl.js
+++ crawl.js
@@ -1,6 +1,17 @@
 const { JSDOM } = require('jsdom');
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+  const baseURLObj = new URL(baseURL);
+  const currentURLObj = new URL(currentURL);
+  if (baseURLObj.hostname !== currentURLObj.hostname) {
+    return pages;
+  }
+  const normalizedCurrentURL = normalizeURL(currentURL);
+  if (pages[normalizedCurrentURL] > 0) {
+    pages[normalizedCurrentURL]++;
+    return pages;
+  }
+  pages[normalizedCurrentURL] = 1;
   console.log(`actively crawling: ${currentURL}`);
 
   try {
@@ -9,7 +20,7 @@ async function crawlPage(currentURL) {
       console.log(
         `error in fetch with status code: ${response.status} on page: ${currentURL}`
       );
-      return;
+      return pages;
     }
 
     const contentType = response.headers.get('content-type');
@@ -17,13 +28,21 @@ async function crawlPage(currentURL) {
       console.log(
         `non html response, content type: ${response.status}, on page: ${currentURL}`
       );
+      return pages;
     }
-    console.log(await response.text());
+    const htmlBody = await response.text();
+
+    const nextURLs = getURLsfromHTML(htmlBody, baseURL);
+
+    for (const nextURL of nextURLs) {
+      pages = await crawlPage(baseURL, nextURL, pages);
+    }
   } catch (err) {
     console.log(
       `error in fetch: ${err.message}, on page: ${currentURL}`
     );
   }
+  return pages;
 }
 
 function getURLsfromHTML(htmlbody, baseURL) {
